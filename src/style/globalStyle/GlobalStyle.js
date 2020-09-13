209eba37ca94eddd2d3500fb1f565e5ab9831c92
Matt <matthave@gmail.com>
2020-09-13 22:39:03 +0200

    Initial commit, create basic structure of app, add background for app

---
+++ src/style/globalStyle/GlobalStyle.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+import background from "../../IMG/backgroundApp.jpg";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin:0;
+    padding:0;
+    font-family: Arial, Helvetica, sans-serif;
+    box-sizing:border-box;
+    border:none;
+    outline:0;
+  }
+
+  html{
+    font-size:10px;
+  }
+
+  body{
+    position:relative;
+    min-height:100vh;
+    background-image: url(${background});
+    background-position:center;
+    background-size:cover;
+    z-index:0;
+
+    &:before{
+  content: "";
+  position: absolute;
+  width:50%;
+  height:100%;
+  background-color: rgba(100,100,100,0.3);
+  box-shadow: 0 0 15px 1px black;
+  z-index:1 
+    }
+  }
+`;
+
+export default GlobalStyle;
