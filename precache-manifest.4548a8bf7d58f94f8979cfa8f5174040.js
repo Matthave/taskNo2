12eaae42482354306151b69afda8bac4e9a44ce4
Matt <matthave@gmail.com>
2020-09-14 23:41:50 +0200

    Updates

---
+++ precache-manifest.4548a8bf7d58f94f8979cfa8f5174040.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8d1ce8265372f9992a05594888525f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8985b83511dfa21296f6",
+    "url": "/static/js/2.f6081597.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6081597.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16ec642763cf5dba29dd",
+    "url": "/static/js/main.ec6a06a8.chunk.js"
+  },
+  {
+    "revision": "06d744e540c8bc9cc7c3",
+    "url": "/static/js/runtime-main.c03f4e0e.js"
+  },
+  {
+    "revision": "8f5e10da0268518cb17c670a77fab49e",
+    "url": "/static/media/backgroundApp.8f5e10da.jpg"
+  }
+]);
